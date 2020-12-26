cacc11f5046af7fb2f9d157892282bf83173778b
boddu kishore kumar <kishore@kernelmasters.org>
2020-12-26 21:07:24 +0530

    Release km-bbb-debian-10.3 version

--- prebuilt/km-bbb-sdcard-prebuilt-debian10.sh
+++ prebuilt/km-bbb-sdcard-prebuilt-debian10.sh
@@ -135,15 +135,15 @@ debian_fs_copy_sd()
 	else
 		sudo mount ${media}1 /mnt/rootfs
 	fi
-	if [ -f km-bbb-debian10.2.tar.gz ] ;then
-		echo "km-bbb-debian10.1.tar.gz found"
+	if [ -f km-bbb-debian10.3.tar.gz ] ;then
+		echo "km-bbb-debian10.3.tar.gz found"
 	else
-		echo "km-bbb-debian10.1.tar.gz not found and download from kmserver"
-		echo -e "${Red}wget http://142.93.218.33/elinux/km-bbb-debian10.2.tar.gz${NC}${Purple}"
-		wget http://142.93.218.33/elinux/km-bbb-debian10.2.tar.gz
+		echo "km-bbb-debian10.3.tar.gz not found and download from kmserver"
+		echo -e "${Red}wget http://142.93.218.33/elinux/km-bbb-debian10.3.tar.gz${NC}${Purple}"
+		wget http://142.93.218.33/elinux/km-bbb-debian10.3.tar.gz
 	fi
-	echo -e "${Red}sudo tar -xvf km-bbb-debian10.1.tar.gz -C /mnt/rootfs/${NC}"
-	sudo tar -xvf km-bbb-debian10.2.tar.gz -C /mnt/rootfs/
+	echo -e "${Red}sudo tar -xvf km-bbb-debian10.3.tar.gz -C /mnt/rootfs/${NC}"
+	sudo tar -xvf km-bbb-debian10.3.tar.gz -C /mnt/rootfs/
 	echo -e "${Red}syncing${NC}"
 	sync
 	unmount_all_drive_partitions
